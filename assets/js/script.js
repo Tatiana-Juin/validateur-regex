db0463a04f9a81e78575594579c28d3d3652ff04
Tatiana-Juin <username@users.noreply.github.com>
2026-01-08 12:33:38 +0100

    J'ai commencer a selectionner les elements du dom

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// Seclectionner les element du dom
+let toggleBiblio = document.querySelector(".toggle-bibliotheque");
+
+let hidenBiblio = document.querySelector(".hiden-bibliotheque");
+let btnTelBiblio = document.querySelector(".btn-tel-bibliotheque");
+let btnEmailBiblio = document.querySelector(".btn-email-bibliotheque");
+
+let btnLancer = document.querySelector("#btn-lancer");
+let toggleScore = document.querySelector(".toggle-score");
+let pourcentage = document.querySelector(".div-pourcentage");
+let paraPourcentage = document.querySelector(".pourcentage");
+
+let inputRegex = document.querySelector(".input-regex");
+let btnTelTest = document.querySelector(".btn-tel-test");
+let btnEmailTest = document.querySelector(".btn-email-test")
+let textareaTest = document.querySelector(".test")
+let btnEffacer = document.querySelector(".btn-effacer");
+let btnCopier = document.querySelector(".btn-copier");
+
+// console.log( toggleBiblio)
+// console.log(hidenBiblio)
+// console.log(btnTelBiblio)
+// console.log(btnEmailBiblio)
+// console.log(btnLancer)
+// console.log(toggleScore)
+// console.log(pourcentage)
+// console.log(paraPourcentage)
+// console.log(inputRegex)
+// console.log(btnTelTest)
+// console.log(btnEmailTest)
+// console.log(textareaTest)
+// console.log(btnEffacer)
+// console.log(btnCopier)
